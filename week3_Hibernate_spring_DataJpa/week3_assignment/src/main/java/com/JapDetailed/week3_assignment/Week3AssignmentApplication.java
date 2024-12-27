2323cc732a36ed3899b5466385c5d25419644cc3
Venu99129 <venumadhavtammaala@gmail.com>
2024-12-28 01:06:33 +0530

    second commit

---
+++ week3_Hibernate_spring_DataJpa/week3_assignment/src/main/java/com/JapDetailed/week3_assignment/Week3AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.JapDetailed.week3_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3AssignmentApplication.class, args);
+	}
+
+}
---
+++ week3_Hibernate_spring_DataJpa/week3_assignment/src/test/java/com/JapDetailed/week3_assignment/Week3AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JapDetailed.week3_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
