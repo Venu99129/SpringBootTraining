2390f95162159b4b3092ee41624761cf995b5539
Venu99129 <venumadhavtammaala@gmail.com>
2025-01-14 16:46:28 +0530

    Update ProductReadyFeaturesApplicationTests.java

--- week4_productionReady_features/productReadyFeatures/src/test/java/com/week4/productReadyFeatures/ProductReadyFeaturesApplicationTests.java
+++ week4_productionReady_features/productReadyFeatures/src/test/java/com/week4/productReadyFeatures/ProductReadyFeaturesApplicationTests.java
@@ -6,8 +6,43 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductReadyFeaturesApplicationTests {
 
+	@Autowired
+	private EmployeeClient employeeClient;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	@Order(3)
+	void getAllEmployeesTest(){
+		List<EmployeeDTO> employeeDTOList = employeeClient.getAllEmployees();
+		System.out.println(employeeDTOList);
+	}
+
+	@Test
+	@Order(2)
+	void getEmployeeByIdTest(){
+		Long employeeId = 2L;
+		EmployeeDTO employeeDTO = employeeClient.getEmployeeById(employeeId);
+		System.out.println(employeeDTO);
+	}
+
+	@Test
+	@Order(1)
+	void createEmployeeTest(){
+		EmployeeDTO employeeDTO = EmployeeDTO.builder()
+				.name("anand")
+				.age(2)
+				.email("anand@madhav.com")
+				.role("USER")
+				.salary(23400.90)
+				.dateOfJoining(LocalDate.of(2023, 5,15))
+				.isActive(true)
+				.build();
+
+		EmployeeDTO createdEmployee = employeeClient.createNewEmployee(employeeDTO);
+
+		System.out.println(createdEmployee);
+	}
 }
